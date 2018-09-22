7491db086dd3d15373ef6d168083534a39852312
TheCrazyT <TheCrazyT@users.noreply.github.com>
2018-09-22 15:26:00 +0200

    binary files do not belong to git (in my opinion).
    Moving files to bintray, and use travis-ci to pack project.

---
+++ .bintray.js
@@ -0,0 +1,24 @@
+{
+    "package": {
+        "name": "BlenderMhwModelImporter",
+        "repo": "BlenderMhwModelImporter",
+        "subject": "thecrazyt",
+        "vcs_url": "https://travis-ci.org/TheCrazyT/BlenderMhwModelImporter",
+        "licenses": ["MIT"]
+    },
+
+    "version": {
+        "name": "0.1"
+    },
+
+    "files":
+        [
+            {
+                "includePattern": "/home/travis/build/TheCrazyT/BlenderMhwModelImporter/build/(.*)", "uploadPattern": "0.1/$1",
+                "matrixParams": {
+                    "override": 1 
+                }
+            }
+        ],
+    "publish": true
+}
